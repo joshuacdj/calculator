a2f7503a013e4d8155715f14b2afcaf8fc0d585b
joshuacdj <joshuacheungdj@gmail.com>
2024-06-07 10:48:09 +0800

    Added operator buttons event listener and several helper functions needed to display the current calculation

--- script.js
+++ script.js
@@ -25,9 +25,6 @@ function divide(a, b) {
     return a / b;
 }
 
-let num1 = 0;
-let num2 = 0;
-let op = "";
 
 function operate(num1, num2, op) {
 
@@ -37,14 +34,135 @@ function operate(num1, num2, op) {
             return add(num1, num2);
         case "-":
             return subtract(num1,num2);
-        case "*":
+        case "x":
             return multiply(num1, num2);
         default:
             return divide(num1, num2);
     }
 }
 
+function isOp(op) {
+
+    return (op === "+" || op === "-" || op === "÷" || op === "x");
+}
+
+function calcToArray(calc) {
+
+    let start = 0;
+    let end = 0;
+
+    // Stop when the operator is met
+    while (!isOp(calc[end])) {
+
+        end++;
+    }
+
+    let num1 = calc.slice(start, end - 1);
+
+    // skip the whitespace as well and on to the start of num2
+    start = end + 2;
+
+    let num2 = calc.slice(start, calc.length);
+    let arr = [num1, num2];
+
+    return arr;
+}
+
+
+let op = "";
 let currCalc = "";
+let prevCalc = "";
+
+
+// Function to check whether most recently pressed was a number or not
+function isRecentNumber(currCalc) {
+
+    const num = "0123456789";
+
+    return num.includes(currCalc[currCalc.length - 1]);
+}
+
+// Function to check whether there is an operator or not
+
+function containsOp(currCalc) {
+
+    const ops = "+-÷x";
+
+    for (let i = 0; i < currCalc.length; i++) {
+
+        if (ops.includes(currCalc[i])) {
+            return true;
+        }
+    }
+
+    return false;
+
+}
+
+// Function to check if most recently pressed was an operator or not
+
+function isRecentOp(currCalc) {
+
+    const ops = "+-÷x";
+
+    return ops.includes(currCalc[currCalc.length - 1]);
+}
+
+const numberBtns = document.querySelectorAll(".number");
+
+const currDisplay = document.querySelector(".curr-calc");
+
+// Number buttons event listener
+numberBtns.forEach(numBtn => {
+
+    numBtn.addEventListener("click", event => {
+
+        let num = numBtn.textContent;
+
+        if (isRecentOp(currCalc)) {
+            currCalc += " ";
+        }
+
+        currCalc += num;
+
+        currDisplay.textContent = currCalc;
+    });
+});
+
+// Operator buttons event listener
+const opBtns = document.querySelectorAll(".operator");
+
+opBtns.forEach(opBtn => {
+
+    opBtn.addEventListener("click", event => {
+
+        op = opBtn.textContent;
+
+        // If there is already an operator AND if recent is a number, pressing anymore operators should do nothing
+        if (containsOp(currCalc) && isRecentNumber(currCalc)) {
+            /* BLANK */
+        } else if (isRecentOp(currCalc)) { // prevent multiple operators in succession
+            currCalc = currCalc.substring(0, currCalc.length - 1);
+            currCalc += " " + op;
+        } else {
+            currCalc += " " + op;
+        }
+
+    
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
